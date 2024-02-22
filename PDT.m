26775eb11a11b8d146754ea999affceac2cbf080
Zhara Nabila <96189079+zarajeong@users.noreply.github.com>
2024-02-22 18:08:03 +0700

    Add files via upload

---
+++ PDT.m
@@ -0,0 +1,54 @@
+% Persamaan Differensial
+
+function [dxdt] = PDT(tt,x,T,PH2S,PO)
+
+kT = 8.625e-5*(165+273); %Boltzmann
+sigmaPt = 1.3E+19; %material Pt
+mH2S = 5.66e-26; %massa H2S
+mO = 2.6560e-26; %massa O
+mSO2 = 1.0240e-25; %massa SO2
+mH2O = 2.98897e-26; %massa H2O
+
+% TotalX adalah total coverage
+TotalX = x(1) + x(2) + x(3) + x(4); %H2S + 3O -> SO2 + H2O 
+% Material yang digunakan sementara adalah Pt (k = 1.38E-23 J)
+fH2S = (1-TotalX)*PH2S*1.01E+5/(sigmaPt*sqrt(2*pi*mH2S*1.38E-23*T)); %(2.12)
+fO = ((1-TotalX)^2)*PO*1.01E+5/(sigmaPt*sqrt(2*pi*mO*1.38E-23*T)); %(2.14)
+
+% Untuk rf1 (H2S)
+Sx1 = 2.18;  
+Ef1 = 0.0905; 
+rf1 = Sx1*exp(-Ef1/kT);
+
+% Untuk rr1 (H2S)
+vr1 = 9.5e+12; 
+Er1 = 0.715; 
+rr1 = vr1*exp(-Er1/kT);
+
+% Untuk rf2 (O)
+Sx2 = 0.423; 
+Ef2 = 0.015; 
+rf2 = Sx2*exp(-Ef2/kT);
+
+% Untuk rr2 (O)
+vr2 = 1.425e+12; 
+Er2 = 0.158; 
+rr2 = vr2*exp(-Er2/kT);
+
+% Untuk rf3 (H2S dan O)
+vf3 = 3.25e+12;  
+Ef3 = 0.368;   
+rf3 = vf3*exp(-Ef3/kT);
+
+% Untuk rr3 (H2S dan O)
+vr3 = 2.05e+12;    
+Er3 = 0.064;
+rr3 = vr3*exp(-Er3/kT);
+
+% Nilai differensial
+dH2S_dt = rf1*fH2S - rr1*x(1) + rr3*x(3)*x(4) - rf3*x(1)*x(2)^3;
+dO_dt   = 2*rf2*fO - 2*rr2*x(2)^2 + rr3*x(3)*x(4) - rf3*x(1)*x(2)^3;
+dSO2_dt = rf3*x(1)*x(2)^3 - rr3*x(3)*x(4);
+dH2O_dt = rf3*x(1)*x(2)^3 - rr3*x(3)*x(4);
+dxdt    = [dH2S_dt; dO_dt; dSO2_dt; dH2O_dt];
+end
---
+++ SA.m
@@ -0,0 +1,131 @@
+%Optimasi Parameter
+
+clear;
+options = odeset('MaxStep',0.5,'RelTol',1e-6,'AbsTol',[1e-6,1e-6,1e-6,1e-6],'NonNegative',[1 2 3 4]);
+    
+%Suhu dalam Kelvin
+Temp=165+273;
+% Nilai awal (variasi)
+nn=1;
+% Er1=0;
+% Er1part=0.005; 
+
+% Looping 
+% while Er1<=1
+    %waktu 45 menit
+         t=0;
+         t0=t;
+         tmax=45*60; %waktu akhir
+    %inisialisasi variabel
+         p=1;
+         tpart=10; %loncatan
+         TotalError=0; 
+         TotalData=0;
+         sigmaPt=1.31e+19;
+    %Variabel coverage 
+         x1=0;
+         x2=0;
+         x3=0;
+         x4=0;
+         
+    %Input P diubah berdasarkan t
+    while t<=tmax
+          if and(t>=t0,t<t0+15*60) 
+             PH2S=0/1E+6;
+          elseif and(t>=t0+15*60,t<t0+30*60) 
+                 PH2S=25/1E+6;
+          elseif and(t>=t0+30*60,t<=t0+45*60) 
+                 PH2S=0/1E+6;
+          end
+
+        %Nilai PO bergantung pada nilai H2S
+        PO=0.20*(1-PH2S); %di data PO = 0 Pa
+        T=Temp;
+        
+        % Penyelesaian PDB 
+        [tt,x] = ode23tb(@(tt,x)PDT(tt,x,T,PH2S,PO),[t t+tpart],[x1 x2 x3 x4],options);
+        pmax = p+length(tt)-1;
+        n=1;
+      
+        while p~=pmax 
+              H2S(p)=x(n,1); 
+              O(p)=x(n,2);
+              SO2(p)=x(n,3); 
+              H2O(p)=x(n,4);
+              Total(p)=H2S(p)+O(p)+SO2(p)+H2O(p);
+        
+            % WF berbanding lurus dengan nilai coverage
+            WFH2S(p)=H2S(p); 
+            WFO(p)=O(p); 
+            WFSO2(p)=SO2(p);
+            WFH2O(p)=H2O(p);
+            WFC(p)= -(WFH2S(p)+WFO(p)+WFSO2(p)+WFH2O(p))+0.0147;
+       
+              if and(tt(n)>=0,tt(n)<2710) 
+                  Intercept=0.00144; 
+                  a=1.0196E-4;
+                  b=-7.97974E-7;
+                  c=2.68259E-9;
+                  d=-4.45246E-12;
+                  e=4.01128E-15;
+                  f=-2.07654E-18;
+                  g=6.19422E-22;
+                  h=-9.91165E-26;
+                  i=6.59227E-30;
+                  WFCeks(p)=Intercept + a*tt(n) + b*tt(n)^2 + c*tt(n)^3 + d*tt(n)^4 + e*tt(n)^5 + f*tt(n)^6 + g*tt(n)^7 + h*tt(n)^8 + i*tt(n)^9;
+            end
+            
+           time(p)=tt(n);
+           TotalError = TotalError+abs(WFC(p)-WFCeks(p));
+           n=n+1;
+           p=p+1;
+        end
+
+        p=p-1;
+            t=t+tpart;
+                t/45;
+        x1=x(n-1,1);
+        x2=x(n-1,2); 
+        x3=x(n-1,3);
+        x4=x(n-1,4);
+    end
+
+    AverageError(nn)=TotalError/p;
+    figure; 
+    % EEr1(nn)=Er1; 
+    %     Er1=Er1+Er1part;
+    %          nn=nn+1;
+% disp(TotalError)
+disp(AverageError)
+% disp(nn)
+% disp(Ef1)
+% end
+
+  % plot(EEr1,AverageError,'c','LineWidth',1.25) %gcm
+  %     xlabel('Er1 (eV)');
+  %         ylabel('Average Error (V)');
+          
+     % plot(time/60, WFCeks, 'g', time/60, WFC, 'r','LineWidth',1.25);
+     %     xlabel('Time (m)');
+     %        ylabel('CPD (V)');
+
+     plot(time/60, WFO, 'g', 'LineWidth',1.25);
+         xlabel('Time (m)');
+            ylabel('CPD (V)');
+
+    fid=fopen('g1.pdf', 'w'); 
+         y=(time/60); 
+             fprintf(fid,'%20.18f\n',y);
+                 fclose(fid);
+     fid=fopen('g2.pdf', 'w'); 
+         y=(WFO); 
+             fprintf(fid,'%20.18f\n',y);
+                 fclose(fid);
+   %          fid=fopen('c.pdf', 'w'); 
+   %            y=(time/60); 
+   %                fprintf(fid,'%20.18f\n',y);
+   %                    fclose(fid);
+   %        fid=fopen('d.pdf', 'w'); 
+   %            y=(WFC); 
+   %                fprintf(fid,'%20.18f\n',y);
+   %                    fclose(fid);
